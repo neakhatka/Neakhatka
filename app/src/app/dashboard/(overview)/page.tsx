bfcdd39bd6bcc163b8fbcf5011a86beb20afa62d
SOMKEY\ROG <manithjr1331@gmail.com>
2024-06-01 19:43:10 +0700

    fix dashboard

--- app/src/app/dashboard/(overview)/page.tsx
+++ app/src/app/dashboard/(overview)/page.tsx
@@ -36,10 +36,10 @@ export default function Page() {
           <CardPostJob />
           <CardJobActive />
         </div>
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           <BarChart />
           <DataTable />
-        </div> */}
+        </div>
       </div>
     </main>
  ) : null;
