fcddf2e006c73832884d108bb35920fb5a9f2dbe
SANVISAL <sanvisal2302@gmail.com>
2024-06-15 16:53:19 +0700

    change post model

--- packages/company-service/src/database/@types/post-interface.ts
+++ packages/company-service/src/database/@types/post-interface.ts
@@ -2,18 +2,18 @@ import mongoose from "mongoose";
 
 export interface IpostDocument {
   companyId?: mongoose.Schema.Types.ObjectId;
-  title?: string;
-  description?: string;
-  requirements?: string[];
-  responsibilities?: string[]; 
-  people?: number;
+  companyName?: string;
+  workplace?: string;
   location?: string;
-  duration?: number;
-  gender: "male" | "female" | "other";
-  type: "full-time" | "part-time";
-  available_position?: number;
-  language?: string[];
-  deadline?: Date;
-  salaries?: number[];
+  jobDescription?: string[];
+  jobResponsibilities?: string[]; 
+  startDate: string;
+  endDate:string;
+  salary?: number[];
+  totalEmployees?: number;
+  time: "full-time" | "part-time";
+  duration?: string;
+  availablePositions?: number;
+  gender: string;
   createdAt: Date;
 }
