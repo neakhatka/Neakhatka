7a13fca6c257cbacb593a8fa4680a72745b99cd9
SOMKEY\ROG <manithjr1331@gmail.com>
2024-06-16 16:40:31 +0700

    complated fetch compant data

---
+++ app/src/Types/CompanyProfile.tsx
@@ -0,0 +1,12 @@
+export interface ICompanyProfile {
+  companyname?: string;
+  logo?: string;
+  contactphone?: string;
+  contactemail?: string;
+  websitelink?: string;
+  location?: string;
+  contactPerson?: string;
+  numberOfemployees?: number[];
+  address?: string;
+  companydescription?: string;
+}
