8dc920af0dd64dbf95238bb5af49ec5848a83adc
Rathnachh <rathna.chh@gmail.com>
2024-06-27 16:40:35 +0700

    env company

--- app/src/app/(profile)/seeker-profile/page.tsx
+++ app/src/app/(profile)/seeker-profile/page.tsx
@@ -1,13 +1,62 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Image from "next/image";
 import { Card, Icon } from "@/components";
 import { Typography } from "../../../components/atoms/Typography";
-import CardList from "@/components/molecules/Card/CardList";
-
 import { Button } from "@/components/ui/button";
-import { DetailCard } from "@/Types/DetailCard";
 
-const seekerProfile = () => {
+interface Seeker {
+  name: string;
+  email?: string;
+  dateOfBirth?: string;
+  contactNumber?: string;
+  nationality?: string;
+  gender?: string;
+  location?: string;
+  address?: string;
+  educationBackground?: string[];
+}
+
+const SeekerProfile = () => {
+  const [profile, setProfile] = useState<Seeker>({
+    name: "",
+    email: "",
+    dateOfBirth: "",
+    contactNumber: "",
+    nationality: "",
+    gender: "",
+    location: "",
+    address: "",
+    educationBackground: []
+  });
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/v1/users/profile", {
+          withCredentials: true,
+        });
+        console.log("data:", response.data);
+        setProfile(response.data);
+      } catch (error) {
+        console.error("Error fetching profile data:", error);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  const handleEditProfile = async () => {
+    try {
+      await axios.put(`http://localhost:4000/v1/users/profile`, {
+        withCredentials: true,
+      });
+    } catch (error) {
+      console.error("Error update post : ", error);
+    }
+  };
+
   return (
     <>
       <div className="container xl:max-w-[1200px] bg-[#F8F9FA] rounded-xl mt-5 md:mt-10 p-5 md:px-24 md:py-10 mb-10">
@@ -23,15 +72,18 @@ const seekerProfile = () => {
             />
             <div className="ml-4 md:ml-6">
               <Typography className="mb-1 md:mb-2 text-[18px] md:text-[24px]">
-                San Visal
+                {profile.name}
               </Typography>
               <Typography className="text-gray-400 text-[12px] md:text-[18px]">
-                sanvisal@gmail.com
+                {profile.email}
               </Typography>
             </div>
           </div>
           <div className="flex items-center">
-            <Button className="bg-[#4B9960] rounded-full flex items-center justify-center gap-2 px-4 md:px-6 py-2">
+            <Button
+              className="bg-[#4B9960] rounded-full flex items-center justify-center gap-2 px-4 md:px-6 py-2"
+              onClick={handleEditProfile}
+            >
               <Icon label="Edit" className="flex items-center justify-center" />
               <span className="hidden md:block">Edit profile</span>
             </Button>
@@ -42,40 +94,39 @@ const seekerProfile = () => {
           <div className="w-full">
             <div className="my-[25px]">
               <Typography fontSize="lg">Date of Birth</Typography>
-              <Typography>2001/10/13</Typography>
+              <Typography>{profile.dateOfBirth}</Typography>
             </div>
             <div className="my-[25px]">
               <Typography fontSize="lg">Contact Number</Typography>
-              <Typography>0965774927</Typography>
+              <Typography>{profile.contactNumber}</Typography>
             </div>
             <div className="my-[25px]">
               <Typography fontSize="lg">Nationality</Typography>
-              <Typography>Khmer</Typography>
+              <Typography>{profile.nationality}</Typography>
             </div>
             <div className="my-[25px]">
               <Typography fontSize="lg">Gender</Typography>
-              <Typography>Male</Typography>
+              <Typography>{profile.gender}</Typography>
             </div>
           </div>
           <div className="w-full">
             <div className="my-[25px]">
               <Typography fontSize="lg">Location</Typography>
-              <Typography>Phnom Penh</Typography>
+              <Typography>{profile.location}</Typography>
             </div>
             <div className="my-[25px]">
               <Typography fontSize="lg">Address</Typography>
-              <Typography>
-                2972 Westheimer Rd. Santa Ana, Illinois 85486
-              </Typography>
+              <Typography>{profile.address}</Typography>
             </div>
             <div className="my-[25px]">
               <Typography fontSize="lg" className="mb-2">
                 Education Background
               </Typography>
-              <Typography className="mb-2">
-                Royal University of Phnom Penh
-              </Typography>
-              <Typography>Preah Sisowath High School</Typography>
+              {profile.educationBackground?.map((education, index) => (
+                <Typography key={index} className="mb-2">
+                  {education}
+                </Typography>
+              ))}
             </div>
           </div>
         </div>
@@ -84,4 +135,4 @@ const seekerProfile = () => {
   );
 };
 
-export default seekerProfile;
+export default SeekerProfile;
